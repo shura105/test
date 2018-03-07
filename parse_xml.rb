a4a8dd20e78db2f23f5f4ff4c457c46ea0c8e7cf
shura105 <36887795+shura105@users.noreply.github.com>
2018-03-07 23:10:21 +0200

    Update parse_xml.rb

--- parse_xml.rb
+++ parse_xml.rb
@@ -2,8 +2,12 @@ require 'nokogiri'
 require 'net/http'
 uri= URI('https://www.goodreads.com/search/index.xml?key=VfarPOcGDiIukWdlYCiBUQ&q=Jack%20London')
 answer = Net::HTTP.get(uri)
+#puts answer
 doc = Nokogiri::XML(answer)
 
-  puts doc.at_css("name").text
-  puts doc.at_css("title").text
-  puts doc.at_css("image_url").text
+doc.css("work").each do |rez|
+  puts rez.at_css("name").text
+  puts rez.at_css("title").text
+  puts rez.at_css("image_url").text
+  puts "---------------------------------------------------------------------------------------"
+end
