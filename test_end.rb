870e48b507250e05d5c0d89054f49120d9da4852
shura105 <36887795+shura105@users.noreply.github.com>
2018-03-07 23:12:29 +0200

    Update test_end.rb

--- test_end.rb
+++ test_end.rb
@@ -1,1 +1,14 @@
+require 'nokogiri'
+require 'net/http'
+uri= URI('https://www.goodreads.com/search/index.xml?key=VfarPOcGDiIukWdlYCiBUQ&q=Jack%20London')
+answer = Net::HTTP.get(uri)
+#puts answer
+doc = Nokogiri::XML(answer)
+
+doc.css("work").each do |rez|
+  puts rez.at_css("name").text
+  puts rez.at_css("title").text
+  puts rez.at_css("image_url").text
+  puts "---------------------------------------------------------------------------------------"
+end
 
