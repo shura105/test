c15b6b3cefc14103407d4926c38ad6d886ca4a19
shura105 <36887795+shura105@users.noreply.github.com>
2018-03-12 16:37:56 +0200

    Update start_wordpress_continer.sh

--- start_wordpress_continer.sh
+++ start_wordpress_continer.sh
@@ -4,6 +4,7 @@ docker run --detach --name test-wordpress --link test-mysql:mysql wordpress
 #start "test-wordpress" container
 docker start test-wordpress
 
-#Get IP 
+#Get IP and put them as address in a browser
 docker inspect test-wordpress |grep IP
+
 
