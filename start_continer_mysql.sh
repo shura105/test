259c18a135b592c9fd68eef2276c7c302843f56c
shura105 <36887795+shura105@users.noreply.github.com>
2018-03-12 16:22:40 +0200

    Update start_continer_mysql.sh

--- start_continer_mysql.sh
+++ start_continer_mysql.sh
@@ -7,3 +7,7 @@ docker run \
 --volume=/root/docker/test-mysql/conf.d:/etc/mysql/conf.d \
 --volume=/storage/docker/mysql-datadir:/var/lib/mysql \
 mysql
+
+#After reboot system container will not start automaticly
+#To start container
+Docker start test-mysql
